c1c9d3d9230b612b8e503e445ed94b1bbe3f9fc1
Kenji Saito <ken-yo@mbr.nifty.com>
2024-06-14 07:56:35 +0900

    Bumps node modules

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Inter } from 'next/font/google';
+import { isMobile } from 'react-device-detect';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,7 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+        <DndProvider
+          backend={isMobile ? TouchBackend : HTML5Backend}
+          options={isMobile ? {
+            scrollAngleRanges: [
+              { start: 60, end: 120 },
+              { start: 240, end: 300 },
+            ],
+          } : undefined}
+        >{children}</DndProvider>
       </body>
     </html>
   );
