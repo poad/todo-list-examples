6fe6905930b9bc77e67c974dc8c974b18f72d7d3
Kenji Saito <ken-yo@mbr.nifty.com>
2026-09-07 23:11:15 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ fi
 echo ""
 pwd
 
-if ! (pnx pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm -r lint-fix && pnpm -r --if-present build); then
+if ! (pnx pnpm@latest self-update && pnpm install -r && pnpm up -r --include-github-actions && pnpm audit --fix override && pnpm up -r && pnpm -r lint-fix && pnpm -r --if-present build); then
   cd "${CUR}" || exit
   exit 1
 fi
